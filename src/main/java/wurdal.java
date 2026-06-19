26eff65ec630ce4294aee7282e3ca38e76419910
Gabriel <gabriel.gomez@aa.com>
2026-06-19 10:10:41 -0500

    Main loop fix and build gradle

--- src/main/java/wurdal.java
+++ src/main/java/wurdal.java
@@ -1,15 +1,23 @@
-import java.util;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
 public class wurdal {
+    
+    public String currentInput = "";
+    public Set<String> currentSeenWords = new HashSet<String>() {};
+
+
     public static void main(String[] args) {
-
+        
         String currInput = "";
-        Scanner scanner = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in)) {
+            while(!currInput.equals("exit") && scanner.hasNextLine()){
+                currInput = scanner.nextLine();
+                System.out.println("User entered: %s".formatted(currInput));
+            }
+        }
 
-        while(!currInput.equals("exit")){
-            currInput = scanner.nextLine();
-            System.out.println("User entered: %s".formatted(currInput));
-        } 
         System.out.println("Welcome to wurdal!");
     }
 }
