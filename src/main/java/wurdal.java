b7fc6fc849a79e4791e5f6eaec23b2ec147fb985
Gabriel <gabriel.gomez@aa.com>
2026-06-19 09:51:06 -0500

    - Gradlew wrapper for building - Wurdal Entrypoint - Gitignore updates

--- src/main/java/wurdal.java
+++ src/main/java/wurdal.java
@@ -1,5 +1,15 @@
+import java.util;
+
 public class wurdal {
     public static void main(String[] args) {
+
+        String currInput = "";
+        Scanner scanner = new Scanner(System.in);
+
+        while(!currInput.equals("exit")){
+            currInput = scanner.nextLine();
+            System.out.println("User entered: %s".formatted(currInput));
+        } 
         System.out.println("Welcome to wurdal!");
     }
 }
